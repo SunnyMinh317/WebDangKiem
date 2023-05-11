f85808485a1798741124cb401daae11efb82d914
anhensam4 <ahs2k3@gmail.com>
2023-05-11 19:19:18 +0700

    getAllVehicle 2

--- server/controller/Vehicle/vehicleControl.js
+++ server/controller/Vehicle/vehicleControl.js
@@ -2,7 +2,7 @@ import db from "../../db.js";
 
 export const getAllVehicle = (req, res) => {
         var sql =
-         "SELECT c.*, v.licensePlate, v.brand, v.model, date_format(v.manufactureDate, '%Y-%m-%d') as manufactureDate, v.certId, date_format(v.certDate, '%Y-%m-%d') as certDate, v.regionId, v.version, v.usePurpose, v.isCompany FROM (SELECT date_format(dob,'%Y-%m-%d') as dob, name, null as representative, address, phone, ov.* FROM owner o INNER JOIN ownervehicle ov on o.ownerId = ov.ownerId UNION ALL SELECT date_format(dob,'%Y-%m-%d') as dob, name, representative, address, phone, cv.* FROM company c INNER JOIN companyvehicle cv on c.ownerId = cv.ownerId) as c inner JOIN vehicles v on c.licensePlate =  v.licensePlate;"
+         "SELECT temp.*, v.licensePlate, v.brand, v.model, DATE_FORMAT(v.manufactureDate, '%Y-%m-%d') AS manufactureDate, v.certId, DATE_FORMAT(v.certDate, '%Y-%m-%d') AS certDate, v.regionId, v.version, v.usePurpose, v.isCompany FROM ( SELECT DATE_FORMAT(dob, '%Y-%m-%d') AS dob, NAME, NULL AS representative, address, phone, ov.* FROM OWNER o INNER JOIN ownervehicle ov ON o.ownerId = ov.ownerId UNION ALL SELECT DATE_FORMAT(dob, '%Y-%m-%d') AS dob, NAME, representative, address, phone, cv.* FROM company c INNER JOIN companyvehicle cv ON c.ownerId = cv.ownerId ) AS temp INNER JOIN vehicles v ON temp.licensePlate = v.licensePlate INNER JOIN registration r ON r.licensePlate = v.licensePlate INNER JOIN centre cen ON cen.centreId = r.centreId;"
         db.query(sql, function (err, data) {
             if (err) {
               return res.status(500).json(err);
