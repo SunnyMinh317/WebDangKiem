daa24f398fd4453340ffeaa344febdcde827f286
Chu Ngoc Vuong <chungocvuong3@gmail.com>
2023-05-11 18:41:12 +0700

    New getAllVehicle query

--- client/src/pages/center/CenterDSXe.js
+++ client/src/pages/center/CenterDSXe.js
@@ -8,7 +8,7 @@ const CenterDSXe = () => {
         <div className={MainLayoutCSS.container}>
             <HeaderCenter className={MainLayoutCSS.header}/>
             <div className={MainLayoutCSS.contentWrap}>
-                <Table title="DANH SÁCH XE" dataLink="/centre/getAllCentres"/>
+                <Table title="DANH SÁCH XE" dataLink="/vehicle/getAllVehicle"/>
             </div>
         </div>
     )
