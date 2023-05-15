59f17909e2d975d26969d233cb5ec743b056dd43
Chu Ngoc Vuong <chungocvuong3@gmail.com>
2023-05-15 22:18:44 +0700

    Update login logout

--- client/src/pages/admin/DSXe.js
+++ client/src/pages/admin/DSXe.js
@@ -1,19 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import HeaderAdmin from "../../components/HeaderAdmin";
 import MainLayoutCSS from "../style/MainLayout.module.css";
 import Table from "../../components/tables/Table";
 import { ColumnCenterCarList } from "../center/ColumnCenterCarList";
-import axios from "axios";
+import { AuthContext } from "../../context/authContext";
 
 const DSXe = () => {
-       
+    const {currentUser} = useContext(AuthContext)
+    console.log(currentUser);
     return (
-        <div className={MainLayoutCSS.container}>
-            <HeaderAdmin className={MainLayoutCSS.header}/>
-            <div className={MainLayoutCSS.contentWrap}>
-                <Table title="DANH SÁCH XE" dataLink="/vehicle/getAllVehicle" columnSet={ColumnCenterCarList}/>
+            <div className={MainLayoutCSS.container}>
+                <HeaderAdmin className={MainLayoutCSS.header}/>
+                <div className={MainLayoutCSS.contentWrap}>
+                {!currentUser ? 
+                    <div>DIT CU DANG NHAP</div> 
+                    :
+                    <Table title="DANH SÁCH XE" dataLink="/vehicle/getAllVehicle" columnSet={ColumnCenterCarList}/>
+                }
+                </div>
             </div>
-        </div>
     )
 }
 
--- server/index.js
+++ server/index.js
@@ -5,7 +5,8 @@ import bodyParser from "body-parser";
 import centreRoutes from "./routes/centreRoutes.js";
 import vehicleRoutes from "./routes/vehicleRoutes.js";
 import deptRoutes from "./routes/deptRoutes.js";
-import importCSV from "./controller/Department/importCSV.js"
+import importCSV from "./controller/Department/importCSV.js";
+import cookieParser from "cookie-parser";
 
 // import cookiesParser from "cookie-parser"
 
@@ -17,7 +18,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// app.use(cookiesParser)
+app.use(cookieParser());
+
 
 app.use("/centre", centreRoutes);
 app.use("/vehicle", vehicleRoutes);
