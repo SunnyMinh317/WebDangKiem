522792b9450c43cd34711f9aa691bf28047f8504
Chu Ngoc Vuong <chungocvuong3@gmail.com>
2023-05-23 10:40:01 +0700

    Add chart data to front end (not yet fix)

--- client/src/pages/prediction/Chart.js
+++ client/src/pages/prediction/Chart.js
@@ -1,15 +1,37 @@
-import { useState } from "react";
-// import "./App.css";
+import { useState, useEffect } from "react";
 import BarChart from "./BarChart";
-import { UserData } from "./Data";
+import axios from "axios";
+import { CarData } from "./Data";
 
 function Chart() {
-  const [userData, setUserData] = useState({
-    labels: UserData.map((data) => data.year),
+  const [loading, setLoading] = useState(true);
+  const [UserData, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () =>{
+      setLoading(true);
+      try {
+        const response = await axios.get('/dept/getRegByYear');
+        setData(response.data);
+        
+      } catch (error) {
+        console.error(error.message);
+      }
+      setLoading(false);
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(UserData)
+  console.log(CarData)
+
+  const [userDatas, setUserDatas] = useState({
+    labels: CarData.map((data) => data.year),
     datasets: [
       {
-        label: "Users Gained",
-        data: UserData.map((data) => data.userGain),
+        label: "Số xe hết hạn",
+        data: CarData.map((data) => data.new_reg_count),
         backgroundColor: [
           "rgba(75,192,192,1)",
         ],
@@ -20,12 +42,32 @@ function Chart() {
     ],
   });
 
+  // console.log(userDatas.labels);
+
+  // setUserData(
+  //   {
+  //     labels: UserData.map((data) => data.year),
+  //     datasets: [
+  //       {
+  //         label: "Số xe hết hạn",
+  //         data: UserData.map((data) => data.new_reg_count),
+  //         backgroundColor: [
+  //           "rgba(75,192,192,1)",
+  //         ],
+  //         borderColor: "black",
+  //         borderWidth: 1,
+  //         borderRadius: 5,
+  //       },
+  //     ],
+  //   }
+  // )
+
   // IF YOU SEE THIS COMMENT: I HAVE GOOD EYESIGHT
 
   return (
     <div className="App">
       <div style={{ width: 700 }}>
-        <BarChart chartData={userData} />
+        <BarChart chartData={userDatas} />
       </div> 
     </div>
   );
--- client/src/pages/prediction/Data.js
+++ client/src/pages/prediction/Data.js
@@ -1,32 +1,65 @@
-export const UserData = [
+// export const CarData = [
+//     {
+//       id: 1,
+//       "year": 2016,
+//       "new_reg_count": 80000,
+//       userLost: 823,
+//     },
+//     {
+//       id: 2,
+//       "year": 2017,
+//       "new_reg_count": 45677,
+//       userLost: 345,
+//     },
+//     {
+//       id: 3,
+//       "year": 2018,
+//       "new_reg_count": 78888,
+//       userLost: 555,
+//     },
+//     {
+//       id: 4,
+//       "year": 2019,
+//       "new_reg_count": 90000,
+//       userLost: 4555,
+//     },
+//     {
+//       id: 5,
+//       "year": 2020,
+//       "new_reg_count": 4300,
+//       userLost: 234,
+//     },
+//   ];
+
+  export const CarData = [
     {
-      id: 1,
-      year: 2016,
-      userGain: 80000,
+      // id: 1,
+      "year": 2016,
+      "new_reg_count": 80000,
       userLost: 823,
     },
     {
-      id: 2,
-      year: 2017,
-      userGain: 45677,
+      // id: 2,
+      "year": 2017,
+      "new_reg_count": 45677,
       userLost: 345,
     },
     {
-      id: 3,
-      year: 2018,
-      userGain: 78888,
+      // id: 3,
+      "year": 2018,
+      "new_reg_count": 78888,
       userLost: 555,
     },
     {
-      id: 4,
-      year: 2019,
-      userGain: 90000,
+      // id: 4,
+      "year": 2019,
+      "new_reg_count": 90000,
       userLost: 4555,
     },
     {
-      id: 5,
-      year: 2020,
-      userGain: 4300,
+      // id: 5,
+      "year": 2020,
+      "new_reg_count": 4300,
       userLost: 234,
     },
   ];
