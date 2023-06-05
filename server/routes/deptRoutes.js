4cf01cbdf88dca8bea005cf060abee58a5da3d5e
anhensam4 <ahs2k3@gmail.com>
2023-06-05 16:08:26 +0700

    update
    RegByCentre
    establishedDate to yyyy-mm-dd
    getAllVehicleByCentre
    isRegis in getAllVehicle

--- server/routes/deptRoutes.js
+++ server/routes/deptRoutes.js
@@ -1,8 +1,8 @@
 import express from "express"
 import importCSV from "../controller/Department/importCSV.js";
 import db from "../db.js";
-import { getRegByYear } from "../controller/Department/getRegByYear.js";
-import { getRegByMonth } from "../controller/Department/getRegByMonth.js";
+import { getRegByYear, getRegByYearByCentre } from "../controller/Department/getRegByYear.js";
+import { getRegByMonth, getRegByMonthByCentre } from "../controller/Department/getRegByMonth.js";
 
 const router = express.Router();
 
@@ -16,7 +16,9 @@ router.post("/test", (req, res) => {
 })
 
 router.get("/getRegByMonth", getRegByMonth)
+router.get("/getRegByMonthByCentre", getRegByMonthByCentre)
 router.get("/getRegByYear", getRegByYear)
+router.get("/getRegByYearByCentre", getRegByYearByCentre)
 
 
 export default router
--- server/routes/vehicleRoutes.js
+++ server/routes/vehicleRoutes.js
@@ -1,11 +1,13 @@
 import express from "express"
-import { getAllVehicle } from "../controller/Vehicle/vehicleControl.js";
+import { getAllVehicle, getAllVehicleByCentre } from "../controller/Vehicle/vehicleControl.js";
 import db from "../db.js";
 
 
 const router = express.Router();
 
 router.get("/getAllVehicle", getAllVehicle)
+router.get("/getAllVehicleByCentre", getAllVehicleByCentre)
+
 
 
 export default router
