778d2717aa109ce98c7d1f54be7a57fa9122a735
Chu Ngoc Vuong <chungocvuong3@gmail.com>
2023-06-06 22:01:21 +0700

    Update app routes

--- client/src/App.js
+++ client/src/App.js
@@ -13,23 +13,88 @@ import CentreHomePage from './pages/CentreHomePage';
 import AdminHomePage from './pages/AdminHomePage';
 import Dashboard from './pages/prediction/Dashboard';
 import Profile from './pages/center/Profile';
+import Footer from './components/Footer';
+import { Outlet } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const Layout = ()=>{
+  return (
+    <>
+      <Outlet/>
+      <Footer/>
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children:[
+      //! Admin routes
+      {
+        path: '/adminHome',
+         element: <AdminHomePage/>
+      },
+      {
+        path: '/danhsachxe',
+         element: <DSXe/>,
+      },
+      {
+        path: '/danhsachtrungtam',
+         element: <DSTrungTam/>,
+
+      },{
+        path: '/captaikhoan',
+         element: <CapTaiKhoan/>,
+      },
+      //!Centre routes
+      {
+        path: '/centreHome',
+        element: <CentreHomePage/>
+      },
+      {
+        path: '/dsxetrungtam',
+        element: <CenterDSXe/>
+      },
+      {
+        path: '/themxe',
+        element: <AddCar/>
+      },
+      {
+        path: '/dubao',
+        element: <Dashboard/>
+      },
+      {
+        path: '/profile',
+        element: <Profile/>
+      }
+    ]
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  },
+])
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* <Route index element={<HomePage/>}/>   */}
-        <Route path='/adminHome' element={<AdminHomePage/>}/>
-        <Route path='/centreHome' element={<CentreHomePage/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/danhsachxe' element={<DSXe/>}/>
-        <Route path='/danhsachtrungtam' element={<DSTrungTam/>}/>
-        <Route path='/captaikhoan' element={<CapTaiKhoan/>}/>
-        <Route path='/dsxetrungtam' element={<CenterDSXe/>}/>
-        <Route path='/themxe' element={<AddCar/>}/>
-        <Route path='/dubao' element={<Dashboard/>}/>
-        <Route path='/profile' element={<Profile/>}/>
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     {/* <Route index element={<HomePage/>}/>   */}
+    //     <Route path='/adminHome' element={<AdminHomePage/>}/>
+    //     <Route path='/centreHome' element={<CentreHomePage/>}/>
+    //     <Route path='/login' element={<Login/>}/>
+    //     <Route path='/danhsachxe' element={<DSXe/>}/>
+    //     <Route path='/danhsachtrungtam' element={<DSTrungTam/>}/>
+    //     <Route path='/captaikhoan' element={<CapTaiKhoan/>}/>
+    //     <Route path='/dsxetrungtam' element={<CenterDSXe/>}/>
+    //     <Route path='/themxe' element={<AddCar/>}/>
+    //     <Route path='/dubao' element={<Dashboard/>}/>
+    //     <Route path='/profile' element={<Profile/>}/>
+    //   </Routes>
+    // </BrowserRouter>
+    <RouterProvider router={router}/>
   )
 }
 
