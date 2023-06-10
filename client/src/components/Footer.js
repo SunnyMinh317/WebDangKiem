fb07d1ce07e8971819dff261d23aacfae61e659a
Chu Ngoc Vuong <chungocvuong3@gmail.com>
2023-06-10 16:13:28 +0700

    Add footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,57 +1,51 @@
 import React from "react";
 import FooterCSS from "./style/Footer.module.css"
 import { Link } from "react-router-dom";
-// import "./style/Footer.css"
+import "./style/Footer.css"
 
 const Footer = () => {
     return (
-        <body>
-
-  <footer class="footer">
-  	 <div class="container">
-  	 	<div class="row">
-  	 		<div class="footer-col">
-  	 			<h4>company</h4>
-  	 			<ul>
-  	 				<li><a href="#">about us</a></li>
-  	 				<li><a href="#">our services</a></li>
-  	 				<li><a href="#">privacy policy</a></li>
-  	 				<li><a href="#">affiliate program</a></li>
-  	 			</ul>
-  	 		</div>
-  	 		<div class="footer-col">
-  	 			<h4>get help</h4>
-  	 			<ul>
-  	 				<li><a href="#">FAQ</a></li>
-  	 				<li><a href="#">shipping</a></li>
-  	 				<li><a href="#">returns</a></li>
-  	 				<li><a href="#">order status</a></li>
-  	 				<li><a href="#">payment options</a></li>
-  	 			</ul>
-  	 		</div>
-  	 		<div class="footer-col">
-  	 			<h4>online shop</h4>
-  	 			<ul>
-  	 				<li><a href="#">watch</a></li>
-  	 				<li><a href="#">bag</a></li>
-  	 				<li><a href="#">shoes</a></li>
-  	 				<li><a href="#">dress</a></li>
-  	 			</ul>
-  	 		</div>
-  	 		<div class="footer-col">
-  	 			<h4>follow us</h4>
-  	 			<div class="social-links">
-  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
-  	 				<a href="#"><i class="fab fa-twitter"></i></a>
-  	 				<a href="#"><i class="fab fa-instagram"></i></a>
-  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
-  	 			</div>
-  	 		</div>
-  	 	</div>
-  	 </div>
-  </footer>
-
-</body>
+			<footer class="footer">
+				<div class="container">
+					<div class="row">
+						<div class="footer-col">
+							<h4>Cục đăng kiểm</h4>
+							<ul>
+								<li><a href="#">Trang chủ</a></li>
+								<li><a href="#">Danh sách xe</a></li>
+								<li><a href="#">Danh sách trung tâm</a></li>
+								<li><a href="#">Cập nhập dữ liệu</a></li>
+							</ul>
+						</div>
+						<div class="footer-col">
+							<h4>Trung tâm</h4>
+							<ul>
+								<li><a href="#">Danh sách xe</a></li>
+								<li><a href="#">Đăng kiểm</a></li>
+								<li><a href="#">Dự báo đăng kiểm</a></li>
+								<li><a href="#">Trang cá nhân</a></li>
+							</ul>
+						</div>
+						<div class="footer-col">
+							<h4>Thành viên</h4>
+							<ul>
+								<li><a href="#">Chu Ngọc Vượng</a></li>
+								<li><a href="#">Nguyễn Hoàng Ân</a></li>
+								<li><a href="#">Vũ Nhật Minh</a></li>
+							</ul>
+						</div>
+						<div class="footer-col">
+							<h4>follow us</h4>
+							<div class="social-links">
+								<a href="#"><i class="fab fa-facebook-f"></i></a>
+								<a href="#"><i class="fab fa-twitter"></i></a>
+								<a href="#"><i class="fab fa-instagram"></i></a>
+								<a href="#"><i class="fab fa-linkedin-in"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</footer>
     );
 }
 
