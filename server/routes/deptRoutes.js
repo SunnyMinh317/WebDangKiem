ed8e769b5a632bea22c245111b85526f85c698b4
anhensam4 <ahs2k3@gmail.com>
2023-06-13 00:52:12 +0700

    update

--- server/routes/deptRoutes.js
+++ server/routes/deptRoutes.js
@@ -1,15 +1,16 @@
 import express from "express"
 import importCSV from "../controller/Department/importCSV.js";
 import db from "../db.js";
-import { getRegByYear, getRegByYearByCentre } from "../controller/Department/getRegByYear.js";
-import { getRegByMonth, getRegByMonthByCentre } from "../controller/Department/getRegByMonth.js";
+import { getRegByYear } from "../controller/Department/getRegByYear.js";
+import { getRegByMonth } from "../controller/Department/getRegByMonth.js";
+import { getRegByQuarter } from "../controller/Department/getRegByQuarter.js";
 import { getRegisDept } from "../controller/Department/getRegisDept.js";
 const router = express.Router();
 
 router.get("/getRegByMonth", getRegByMonth)
 router.get("/getRegByYear", getRegByYear)
 router.get("/getRegisDept", getRegisDept)
-router.get("/getRegByQuarter", getRegByYear)
+router.get("/getRegByQuarter", getRegByQuarter)
 
 
 export default router
