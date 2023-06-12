bfdb248b97900f809c2e109adb4bc996b53bb01c
anhensam4 <ahs2k3@gmail.com>
2023-06-13 01:50:13 +0700

    update

--- server/controller/Department/getRegByMonth.js
+++ server/controller/Department/getRegByMonth.js
@@ -3,7 +3,7 @@ import { centreId } from "../Centre/getCentreId.js";
 
 export const getRegByMonth = (req, res) => {
   var sql =
-    "WITH expire_reg AS (SELECT calendar.month, IFNULL(reg_count, 0) AS expire_count FROM (SELECT DISTINCT MONTH(expireDate) AS month FROM registration WHERE expireDate < CURRENT_DATE AND YEAR(expireDate) < YEAR(CURRENT_DATE)) AS calendar LEFT JOIN (SELECT MONTH(expireDate) AS month, COUNT(*) AS reg_count FROM registration WHERE YEAR(expireDate) = YEAR(CURRENT_DATE) GROUP BY MONTH(expireDate)) AS regis ON calendar.month = regis.month ORDER BY calendar.month),new_reg AS (SELECT calendar.month, IFNULL(reg_count, 0) AS new_reg_count FROM (SELECT DISTINCT MONTH(registrationDate) AS month FROM registration WHERE registrationDate < CURRENT_DATE AND YEAR(registrationDate) < YEAR(CURRENT_DATE)) AS calendar LEFT JOIN (SELECT MONTH(registrationDate) AS month, COUNT(*) AS reg_count FROM registration WHERE YEAR(registrationDate) = YEAR(CURRENT_DATE) - 1 GROUP BY MONTH(registrationDate)) AS regis ON calendar.month = regis.month ORDER BY calendar.month) SELECT expire_reg.month, expire_reg.expire_count, new_reg.new_reg_count FROM expire_reg JOIN new_reg ON expire_reg.month = new_reg.month ORDER BY MONTH;";
+    "WITH expire_reg AS (SELECT calendar.month, IFNULL(reg_count, 0) AS expire_count FROM (SELECT DISTINCT MONTH(expireDate) AS month FROM registration WHERE expireDate < CURRENT_DATE AND YEAR(expireDate) < YEAR(CURRENT_DATE)) AS calendar LEFT JOIN (SELECT MONTH(expireDate) AS month, COUNT(*) AS reg_count FROM registration WHERE YEAR(expireDate) = YEAR(CURRENT_DATE) GROUP BY MONTH(expireDate)) AS regis ON calendar.month = regis.month ORDER BY calendar.month),new_reg AS (SELECT calendar.month, IFNULL(reg_count, 0) AS new_reg_count FROM (SELECT DISTINCT MONTH(registrationDate) AS month FROM registration WHERE registrationDate < CURRENT_DATE AND YEAR(registrationDate) < YEAR(CURRENT_DATE)) AS calendar LEFT JOIN (SELECT MONTH(registrationDate) AS month, COUNT(*) AS reg_count FROM registration WHERE YEAR(registrationDate) = YEAR(CURRENT_DATE) - 2 GROUP BY MONTH(registrationDate)) AS regis ON calendar.month = regis.month ORDER BY calendar.month) SELECT expire_reg.month, expire_reg.expire_count, new_reg.new_reg_count FROM expire_reg JOIN new_reg ON expire_reg.month = new_reg.month ORDER BY MONTH;";
   db.query(sql, function (err, data) {
     if (err) {
       return res.status(500).json(err);
@@ -12,3 +12,13 @@ export const getRegByMonth = (req, res) => {
   });
 };
 
+export const getStatByMonth = (req, res) => {
+  var sql =
+    "WITH expire_reg AS( SELECT calendar.month, IFNULL(reg_count, 0) AS expire_count FROM ( SELECT DISTINCT MONTH(expireDate) AS month FROM registration WHERE expireDate < CURRENT_DATE AND YEAR(expireDate) < YEAR(CURRENT_DATE) ) AS calendar LEFT JOIN( SELECT MONTH(expireDate) AS month, COUNT(*) AS reg_count FROM registration WHERE YEAR(expireDate) = YEAR(CURRENT_DATE) - 1 GROUP BY MONTH(expireDate) ) AS regis ON calendar.month = regis.month ORDER BY calendar.month ), new_reg AS( SELECT calendar.month, IFNULL(reg_count, 0) AS new_reg_count FROM ( SELECT DISTINCT MONTH(registrationDate) AS month FROM registration WHERE registrationDate < CURRENT_DATE AND YEAR(registrationDate) < YEAR(CURRENT_DATE) ) AS calendar LEFT JOIN( SELECT MONTH(registrationDate) AS month, COUNT(*) AS reg_count FROM registration WHERE YEAR(registrationDate) = YEAR(CURRENT_DATE) - 1 GROUP BY MONTH(registrationDate) ) AS regis ON calendar.month = regis.month ORDER BY calendar.month ) SELECT expire_reg.month, expire_reg.expire_count, new_reg.new_reg_count FROM expire_reg JOIN new_reg ON expire_reg.month = new_reg.month ORDER BY MONTH;";
+  db.query(sql, function (err, data) {
+    if (err) {
+      return res.status(500).json(err);
+    }
+    return res.status(200).json(data);
+  });
+};
--- server/controller/Department/getRegByYear.js
+++ server/controller/Department/getRegByYear.js
@@ -11,3 +11,14 @@ export const getRegByYear = (req, res) => {
     return res.status(200).json(data);
   });
 };
+
+export const getStatByYear = (req, res) => {
+  var sql =
+    "WITH expire_reg AS( SELECT calendar.year, IFNULL(expire_count, 0) AS expire_count FROM ( SELECT DISTINCT YEAR(CURRENT_DATE) - 5 AS YEAR FROM registration UNION SELECT DISTINCT YEAR(CURRENT_DATE) - 4 AS YEAR FROM registration UNION SELECT DISTINCT YEAR(CURRENT_DATE) - 3 AS YEAR FROM registration UNION SELECT DISTINCT YEAR(CURRENT_DATE) - 2 AS YEAR FROM registration UNION SELECT DISTINCT YEAR(CURRENT_DATE) - 1 AS YEAR FROM registration ) AS calendar LEFT JOIN( SELECT YEAR(expireDate) AS YEAR, COUNT(*) AS expire_count FROM registration WHERE YEAR(expireDate) >= YEAR(CURRENT_DATE) - 5 GROUP BY YEAR(expireDate) ORDER BY YEAR(expireDate) LIMIT 5 ) AS EXPIRE ON calendar.year = EXPIRE.year ORDER BY calendar.year ), new_reg AS( SELECT calendar.year, IFNULL(reg_count, 0) AS new_reg_count FROM ( SELECT DISTINCT YEAR(CURRENT_DATE) - 5 AS YEAR FROM registration UNION SELECT DISTINCT YEAR(CURRENT_DATE) -4 AS YEAR FROM registration UNION SELECT DISTINCT YEAR(CURRENT_DATE) -3 AS YEAR FROM registration UNION SELECT DISTINCT YEAR(CURRENT_DATE) -2 AS YEAR FROM registration UNION SELECT DISTINCT YEAR(CURRENT_DATE) -1 AS YEAR FROM registration ) AS calendar LEFT JOIN( SELECT YEAR(registrationDate) AS YEAR, COUNT(*) AS reg_count FROM registration WHERE registrationDate >= DATE_SUB(CURDATE(), INTERVAL 5 YEAR) GROUP BY YEAR(registrationDate) ORDER BY YEAR(registrationDate) LIMIT 5) AS regis ON calendar.year = regis.year ORDER BY calendar.year ) SELECT expire_reg.year, expire_reg.expire_count, new_reg.new_reg_count FROM expire_reg JOIN new_reg ON expire_reg.year = new_reg.year ORDER BY YEAR;";
+  db.query(sql, function (err, data) {
+    if (err) {
+      return res.status(500).json(err);
+    }
+    return res.status(200).json(data);
+  });
+};
